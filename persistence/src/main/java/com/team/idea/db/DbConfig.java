d3aea942c6672352e5e37fee98a25e5a66b27bc4
DGovorov <heavenbox@mail.ru>
2016-11-18 22:21:09 +0200

    parent project, persistence and business-logic submodules

---
+++ persistence/src/main/java/com/team/idea/db/DbConfig.java
@@ -0,0 +1,74 @@
+package com.team.idea.db;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.annotation.Resource;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan("com.team.idea")
+@EnableJpaRepositories("com.team.idea")
+public class DbConfig {
+
+    private static final String PROPERTY_NAME_DATABASE_DRIVER = "org.postgresql.Driver";
+    private static final String PROPERTY_NAME_DATABASE_URL = "jdbc:postgresql://localhost:5432/social";
+    private static final String PROPERTY_NAME_DATABASE_USERNAME = "postgres";
+    private static final String PROPERTY_NAME_DATABASE_PASSWORD = "Postgres";
+
+    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "org.hibernate.dialect.PostgreSQLDialect";
+    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "true";
+    private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "com.team.idea";
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(PROPERTY_NAME_DATABASE_DRIVER);
+        dataSource.setUrl(PROPERTY_NAME_DATABASE_URL);
+        dataSource.setUsername(PROPERTY_NAME_DATABASE_USERNAME);
+        dataSource.setPassword(PROPERTY_NAME_DATABASE_PASSWORD);
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        entityManagerFactoryBean.setPackagesToScan(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN);
+
+        entityManagerFactoryBean.setJpaProperties(hibProperties());
+
+        return entityManagerFactoryBean;
+    }
+
+    private Properties hibProperties() {
+        Properties properties = new Properties();
+        properties.put(PROPERTY_NAME_HIBERNATE_DIALECT, PROPERTY_NAME_HIBERNATE_DIALECT);
+        properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, PROPERTY_NAME_HIBERNATE_SHOW_SQL);
+        return properties;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
+
+}
---
+++ persistence/src/main/java/com/team/idea/repo/UserRepository.java
@@ -0,0 +1,13 @@
+package com.team.idea.repo;
+
+import com.team.idea.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findAll();
+}
